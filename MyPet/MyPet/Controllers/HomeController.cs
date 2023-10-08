461e146dd1e30175e9302fbdd082661621576d86
igcamargo <igcamargo@sni.org.br>
2023-10-08 20:21:20 -0300

    Model criada

--- MyPet/MyPet/Controllers/HomeController.cs
+++ MyPet/MyPet/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace MyPet.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult SobreNos()
         {
             return View();
         }
---
+++ MyPet/MyPet/Models/Consulta.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPet.Models;
+
+public class Consulta
+{
+    public int Id { get; set; }
+
+    [Display(Name ="Nome Completo")]
+    public string? Nome { get; set; }
+
+    [Display(Name ="Nome do Pet")]
+    public string? NomeDoPet { get; set; }
+
+    [Display(Name = "Tipo do seu pet")]
+    public TipoPet TipoPet { get; set; }
+
+    [DataType(DataType.Date), Display(Name ="Data da consulta")]
+    public DateTime DataConsulta { get; set; }
+
+    [DataType(DataType.Time), Display(Name = "Hora da consulta")]
+    public DateTime HoraConsulta { get; set; }
+
+    [Display(Name ="Escolha o tipo de pagamento")]
+    public TipoPagamento TipoPagamento { get; set; }
+
+}
+
+public enum TipoPet
+{
+    Cachorro, Gato, Pássaro, Peixe, Outros
+}
+
+public enum TipoPagamento
+{
+    Cartão, Dinheiro, Pix
+}
+
